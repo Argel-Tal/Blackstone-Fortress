e565ba6dfc1dc20c79f28ef53c7dadf216f30de1
Jack <80669114+Argel-Tal@users.noreply.github.com>
2023-04-14 16:06:47 +1000

    actions converted to while loop handling cascades

--- Blackstone_Auto_Result.py
+++ Blackstone_Auto_Result.py
@@ -21,7 +21,7 @@ def Enemy():
         print('Ur-Ghul')
     #generate dice roll
         roll=random.randint(1,20)
-        status=input("Enter the status: \n\t\n\tHidden\t= 1,\n\tEngaged\t= 2,\n\tClose\t= 3,\n\tOther\t*\n")
+        status=input("Enter the status: \n\t\n\tHidden\t= 1,\n\tEngaged\t= 2,\n\tClose\t= 3,\n\tOther *\t")
         print("roll\t= "+str(roll))
     #conditionals
         if status == '1':
@@ -445,60 +445,89 @@ def Enemy():
 defines the behaviour output for unit
 '''
 def specialActions(result):
-    # make whitespace
-    print()
+    list_actions = ["sneak", "hold", "fall back", "advance", 'aim', 'charge', 'onslaught', 
+                        'alert', 'rush', 'overcharge', 'recharge', 'disrupt', 'regenerate', 
+                        'empower', 'annihilate', 'rapid fire', 'fury', 'pounce'
+                    ] # end action list
     # handle unmatched characters
     if result is None:
             action = ""
+            return(action)
     else: 
-        print("Action\t= ", result, "\n")
         result = str.lower(result)
-    #result = str.lower(input("Enter behaviour result here for a definition: "))
-    if result=="sneak":
-        action = ('Make a move that ends as close as possible to an explorer without entering a hex that is visible to any explorers')
-    elif result=="hold":
-        action = ("Do nothing")
-    elif result=="fall back":
-        action = ("Double this hostile's Move Value when they take this action. If this hostile can make a move that ends in a hex not visible to any explorers, they do so. If they cannot, they attack the closest explorer that is in range and visible to this hostile.")
-    elif result=="advance":
-        action = ("Move towards the closest explorer. Then attack the closest explorer that is in range and visible to this hostile.")
-    elif result=="aim":
-        action = ("Attack the furthest explorer that is in range and visible to this hostile. That attack ignores Cover.")
-    elif result=="charge":
-        action = ("Move towards the closest explorer. Then attack an explorer that is adjacent and visible to this hostile. If there are no explorers adjacent and visible to this hostile, move towards the closest explorer a second time.")
-    elif result=="onslaught":
-        action = ("Attack the closest explorer that is in range of and is visible to this hostile. Then attack the closest explorer that is in range and visible to this hostile (may be a different target)")
-    elif result=="alert":
-        action = ("Increase the Threat Level by 1 and then take an Onslaught action. IF the Threat Level is 3, take an Onslaught action and reroll failed attack rolls for that action instead.")
-    elif result=="rush":
-        action = ("Move towards the closest explorer. The take a Charge action")
-    elif result=="overcharge":
-        action = ("Make an overcharged Plasma Pistol attack against thge closest explorer that is in range of and visible to Obsidius Mallex.")
-    elif result=="recharge":
-        action = ("Remove a Wound counter from this Negavolt Cultist. If the Negavolt Cultist does not have a Wound counter, treat this as an Advance result instead.")
-    elif result=="disrupt":
-        action = ("One unspent activation or Destiny dice chosen by the hostile player is discarded and cannot be spent this turn. If no unspent activation or Destiny dice are available, one explorer chosen by the hostile player suffers a wound.")
-    elif result=="regenerate":
-        action = ("Remove a Wound counter from this Rogue Psyker. If the Rogue Psyker does not have a Wound counter, treat this as an Disrupt result instead.")
-    elif result=="empower":
-        action = ("Place the Empower marker beside the Rogue Psyker. If the Rogue Psyker is already Empowered, treat this result as a Disrupt result instead. The Rogue Psyker remains Empowered until they suffer a Wound or Grevious Wound.")
-    elif result=="annihilate":
-        action = ("Place the Empower marker beside the Rogue Psyker. Then attack an explorer that is adjacent and visible to this hostile. Reroll failed attack rolls for that attack. The Rogue Psyker remains Empowered until they suffer a Wound or Grevious Wound.")
-    elif result=="rapid fire":
-        action = ("Take an Onslaught action. Reroll failed attack rolls for that action.")
-    elif result=="fury":
-        action = ("Take an Onslaught action. Reroll all failed attack rolls for that action.")
-    elif result=="pounce":
-        action = ("If there is an explorer adjacent and visible to this Ur-Ghul, take an Onslaught action. Otherwise take a Charge action. Reroll failed attack rolls for whichever action is taken.")
-    else:
-        action = ""
-    return(action)
+        while result in list_actions:
+            result = str.lower(result)
+            secondaryAction = ""
+            # make whitespace
+            print()
+            print("Action\t= ", str.capitalize(result), "\n")
+            #result = str.lower(input("Enter behaviour result here for a definition: "))
+            if result=="sneak":
+                action = ('Make a move that ends as close as possible to an explorer without entering a hex that is visible to any explorers')
+                secondaryAction = ""
+            elif result=="hold":
+                action = ("Do nothing")
+                secondaryAction = ""
+            elif result=="fall back":
+                action = ("Double this hostile's Move Value when they take this action. If this hostile can make a move that ends in a hex not visible to any explorers, they do so. If they cannot, they attack the closest explorer that is in range and visible to this hostile.")
+                secondaryAction = ""
+            elif result=="advance":
+                action = ("Move towards the closest explorer. Then attack the closest explorer that is in range and visible to this hostile.")
+                secondaryAction = ""
+            elif result=="aim":
+                action = ("Attack the furthest explorer that is in range and visible to this hostile. That attack ignores Cover.")
+                secondaryAction = ""
+            elif result=="charge":
+                action = ("Move towards the closest explorer. Then attack an explorer that is adjacent and visible to this hostile. If there are no explorers adjacent and visible to this hostile, move towards the closest explorer a second time.")
+                secondaryAction = ""
+            elif result=="onslaught":
+                action = ("Attack the closest explorer that is in range of and is visible to this hostile. Then attack the closest explorer that is in range and visible to this hostile (may be a different target)")
+                secondaryAction = ""
+            elif result=="alert":
+                action = ("Increase the Threat Level by 1 and then take an Onslaught action. IF the Threat Level is 3, take an Onslaught action and reroll failed attack rolls for that action instead.")
+                secondaryAction = "onslaught"
+            elif result=="rush":
+                action = ("Move towards the closest explorer. The take a Charge action")
+                secondaryAction = "charge"
+            elif result=="overcharge":
+                action = ("Make an overcharged Plasma Pistol attack against thge closest explorer that is in range of and visible to Obsidius Mallex.")
+                secondaryAction = ""
+            elif result=="recharge":
+                action = ("Remove a Wound counter from this Negavolt Cultist. If the Negavolt Cultist does not have a Wound counter, treat this as an Advance result instead.")
+                secondaryAction = "advance"
+            elif result=="disrupt":
+                action = ("One unspent activation or Destiny dice chosen by the hostile player is discarded and cannot be spent this turn. If no unspent activation or Destiny dice are available, one explorer chosen by the hostile player suffers a wound.")
+                secondaryAction = ""
+            elif result=="regenerate":
+                action = ("Remove a Wound counter from this Rogue Psyker. If the Rogue Psyker does not have a Wound counter, treat this as an Disrupt result instead.")
+                secondaryAction = "disrupt"
+            elif result=="empower":
+                action = ("Place the Empower marker beside the Rogue Psyker. If the Rogue Psyker is already Empowered, treat this result as a Disrupt result instead. The Rogue Psyker remains Empowered until they suffer a Wound or Grevious Wound.")
+                secondaryAction = "disrupt"
+            elif result=="annihilate":
+                action = ("Place the Empower marker beside the Rogue Psyker. Then attack an explorer that is adjacent and visible to this hostile. Reroll failed attack rolls for that attack. The Rogue Psyker remains Empowered until they suffer a Wound or Grevious Wound.")
+                secondaryAction = ""
+            elif result=="rapid fire":
+                action = ("Take an Onslaught action. Reroll failed attack rolls for that action.")
+                secondaryAction = "onslaught"
+            elif result=="fury":
+                action = ("Take an Onslaught action. Reroll all failed attack rolls for that action.")
+                secondaryAction = "onslaught"
+            elif result=="pounce":
+                action = ("If there is an explorer adjacent and visible to this Ur-Ghul, take an Onslaught action. Otherwise take a Charge action. Reroll failed attack rolls for whichever action is taken.")
+                secondaryAction = "onslaught"
+            else:
+                action = ""
+            print(action)
+            result = secondaryAction
+        # end while
+    return()
 
 def mainMeth():
     inp = str(input("do you need help? y/n (1/0)\t"))
     while (inp == "y" or inp == "1"):
         if (inp == "y" or inp == "1"):
-            print(specialActions(Enemy()))
+            specialActions(Enemy())
             print()
             inp = input("do you need help? y/n (1/0)\t")
         else:
